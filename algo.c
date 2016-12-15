41075e678e5c313d9ac52d07726f85686ff04cc5
Alexander <Alexander@m.ru>
2016-12-16 00:10:02 +0300

    Hello world

---
+++ algo.c
@@ -0,0 +1,18 @@
+/**
+ *  Created on: Dec 13, 2016
+ *      Author: alexander
+ */
+
+#include <stdio.h>
+
+void main ()
+{
+	printf("Insert command\n");
+
+	int x;
+	scanf("%d",x);
+
+	printf("result = %d",x);
+
+	return;
+}
---
+++ algo.h
@@ -0,0 +1,13 @@
+/*
+ * algo.h
+ *
+ *  Created on: Dec 13, 2016
+ *      Author: alexander
+ */
+
+#ifndef ALGO_H_
+#define ALGO_H_
+
+
+
+#endif /* ALGO_H_ */
