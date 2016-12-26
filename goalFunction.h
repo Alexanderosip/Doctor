ab594676914991e5d35724aec72020d319503f1c
Maxim Podoprelov <m.podoprelov@initi.ru>
2016-12-27 00:17:23 +0300

    bf

--- goalFunction.h
+++ goalFunction.h
@@ -10,7 +10,7 @@ using namespace std;
 #define NX 3 // кол-во доп. ограничений
 
 extern void generate(vector < vector <float> > Qij, vector < vector <float> > Rik, vector < vector <float> > Edj, vector <float> Vi, vector <int> Oi); // функция генерирует данные
-extern void letsCheck( vector <vector <float> > Qij, vector <vector <float> > Rik, vector <float> Vi, vector <int> Oi);
+extern void letsCheck( vector <vector <float> > Qij, vector <vector <float> > Rik, vector < vector <float> > Edj, vector <float> Vi, vector <int> Oi);
 
 vector <float> calculate_hardware_capacity(vector <float> R, vector <float> Vi, vector <float> Oi);
 bool check_first_unequality(vector < vector <float> > Qij, vector <float> Xj, vector <float> b, int N);
