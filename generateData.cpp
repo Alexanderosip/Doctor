b28d65cca015b95238042d9b88274fe28f31bd2f
Alexander <Alexander@m.ru>
2016-12-26 23:48:38 +0300

    Добавил заполнение Edj

--- generateData.cpp
+++ generateData.cpp
@@ -4,40 +4,28 @@
 #include <vector>
 
 using namespace std;
-#define NH 3 // кол-во физических компьютеров (Размер парка)
-#define NS 5 // кол-во логических серверов
+#define NH 4 // кол-во физических компьютеров (Размер парка)
+#define NS 6 // кол-во логических серверов
 #define NC 5 // кол-во типов ресурсов
-void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vector <float> Vi, vector <int> Oi);
+#define NX 3 // кол-во доп. ограничений
+
 void generate();
+void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vector <float> Vi, vector <int> Oi);
 
-int main(){
-
-    generate();
-    return 0;
-}
-
+//Генерация значений для матриц
 void generate()
 {
-    vector < vector <float> > Qij; // Требования логических серверов по ресурсам
     vector < vector <float> > Rik; // Возможности физических компов по ресурсам
+    vector < vector <float> > Qij; // Требования логических серверов по ресурсам
+    vector < vector <float> > Edj; // Доп. ограничения
     vector <float> Vi; // Затраты на операционку
     vector <int> Oi; // Приоритетные ресурсы
 
     vector <float> row;
     srand (time(NULL));
     float tmp = 0.0;
-    // заполняем Qij
-    for (int i = 0; i < NC; ++i)
-    {
-        for (int j = 0; j < NS; ++j)
-        {
-            row.push_back(rand() % 200 + 800 + tmp);
-        }
-        tmp += rand() % 300;
-        Qij.push_back(row);
-        row.clear();
-    }
-    // заполняем Rik
+
+    // заполняем матрицу Rik
     for (int i = 0; i < NC; ++i)
     {
         for (int j = 0; j < NH; ++j)
@@ -48,34 +36,48 @@ void generate()
         Rik.push_back(row);
         row.clear();
     }
-    // заполняем Vi
-    for (int i = 0; i < NC; ++i)
-    {
-        Vi.push_back(rand() % 100);
-    }
-    // заполняем Oi
-    for (int i = 0; i < NC; ++i)
-    {
-        Oi.push_back(rand() % 2);
-    }
-    letsCheck(Rik, Qij, Vi, Oi);
-}
 
-void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vector <float> Vi, vector <int> Oi)
-{
-    // печатаем Q
-    printf("Это матрица логических серверов: \n\n");
+    // заполняем матрицу Qij
     for (int i = 0; i < NC; ++i)
     {
         for (int j = 0; j < NS; ++j)
         {
-            // cout << Qij[i][j] << " ";
-            printf("%.1f ", Qij[i][j]);
+            row.push_back(rand() % 200 + 800 + tmp);
         }
-        // cout << endl;
-        printf("\n");
+        tmp += rand() % 300;
+        Qij.push_back(row);
+        row.clear();
     }
-    printf("\n");
+
+    // заполняем матрицу Eij
+    for (int i = 0; i < NX; ++i)
+    {
+        for (int j = 0; j < NS; ++j)
+        {
+            row.push_back(rand() % 200 + 800 + tmp);
+        }
+        tmp += rand() % 300;
+        Edj.push_back(row);
+        row.clear();
+    }
+
+    // заполняем вектор Vi
+    for (int i = 0; i < NC; ++i)
+    {
+        Vi.push_back(rand() % 100);
+    }
+
+    // заполняем вектор Oi
+    for (int i = 0; i < NC; ++i)
+    {
+        Oi.push_back(rand() % 2);
+    }
+
+}
+
+//Вывод сгенерированных матриц и векторов на стандартный вывод
+void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vector <float> Vi, vector <int> Oi)
+{
     // печатаем R
     printf("Это матрица физических компов: \n\n");
     for (int i = 0; i < NC; ++i)
@@ -90,7 +92,35 @@ void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vecto
     }
     printf("\n");
 
-    printf("Это вектор требований операционок физических компов: \n");
+    // печатаем Q
+    printf("Это матрица логических серверов: \n\n");
+    for (int i = 0; i < NC; ++i)
+    {
+        for (int j = 0; j < NS; ++j)
+        {
+            // cout << Qij[i][j] << " ";
+            printf("%.1f ", Qij[i][j]);
+        }
+        // cout << endl;
+        printf("\n");
+    }
+    printf("\n");
+
+    // печатаем E
+    printf("Это матрица доп. ограничений: \n\n");
+    for (int i = 0; i < NX; ++i)
+    {
+        for (int j = 0; j < NS; ++j)
+        {
+            // cout << Qij[i][j] << " ";
+            printf("%.1f ", Qij[i][j]);
+        }
+        // cout << endl;
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("Это вектор требований базовых ОС: \n");
     for (int i = 0; i < NC; ++i)
     {
         // cout << Vi[i] << " ";
@@ -99,7 +129,7 @@ void letsCheck(vector <vector <float> > Rik, vector <vector <float> > Qij, vecto
     // cout << endl;
     printf("\n");
 
-    printf("Это вектор приоритетных ресурсов физических компов: \n");
+    printf("Это вектор приоритетных ресурсов: \n");
     for (int i = 0; i < NC; ++i)
     {
         // cout << Oi[i] << " ";
